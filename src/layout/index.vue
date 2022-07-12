54e5cf8467413cb9a4214ffa6c01fa4baa280d74
MessiHL <roadsclub@163.com>
2022-07-12 14:18:54 +0800

    feat:base

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,7 +3,7 @@
  * @Autor: hl
  * @Date: 2022-07-11 17:21:55
  * @LastEditors: hl
- * @LastEditTime: 2022-07-11 17:55:05
+ * @LastEditTime: 2022-07-12 14:06:38
 -->
 <template>
 	<el-container>
@@ -11,7 +11,15 @@
 		<el-container>
 			<el-header> <Header></Header> <Tabs></Tabs> </el-header>
 			<el-main>
-				<section class="main-box">mainsection</section>
+				<section class="main-box">
+					<router-view v-slot="{ Component, route }">
+						<transition appear name="fade-tarnsform" mode="out-in">
+							<keep-alive :include="cacheRouter">
+								<component :is="Component" :key="route.path"></component>
+							</keep-alive>
+						</transition>
+					</router-view>
+				</section>
 			</el-main>
 			<el-footer><Footer></Footer></el-footer>
 		</el-container>
@@ -22,6 +30,7 @@ import Menu from "./Menu/index.vue";
 import Header from "./Header/index.vue";
 import Tabs from "./Tabs/index.vue";
 import Footer from "./Footer/index.vue";
+import cacheRouter from "@/routers/cacheRouter";
 </script>
 <style lang="scss">
 @import "./index.scss";
