e0868477f3fca2c5cb1ed9306f3542c123bb8560
MessiHL <roadsclub@163.com>
2022-07-12 15:30:18 +0800

    feat:nprogress 添加

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,19 +3,22 @@
  * @Autor: hl
  * @Date: 2022-07-11 15:18:31
  * @LastEditors: hl
- * @LastEditTime: 2022-07-11 15:43:40
+ * @LastEditTime: 2022-07-12 15:29:15
  */
 import router from "@/routers/router";
+import NProgress from "@/config/nprogress";
 
 //  路由拦截
 router.beforeEach((to, from, next) => {
 	// TODO 清除请求 权限判断 token 判断
+	NProgress.start();
 	console.log(to, from, next);
 	return next();
 });
 
 router.afterEach(() => {
 	// TODO  跳转完成后续
+	NProgress.done();
 });
 
 export default router;
